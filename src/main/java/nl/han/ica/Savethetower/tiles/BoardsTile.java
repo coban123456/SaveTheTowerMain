7f30355c41a58da45a8101cd1ff6edeac957b6a1
Ahmet <ahmet.turkan@hotmail.nl>
2017-10-25 20:13:56 +0200

    Begin

---
+++ src/main/java/nl/han/ica/Savethetower/tiles/BoardsTile.java
@@ -0,0 +1,18 @@
+package nl.han.ica.Savethetower.tiles;
+
+//package nl.han.ica.Savethetower.tiles;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+import nl.han.ica.OOPDProcessingEngineHAN.Tile.Tile;
+
+/**
+ * Created by ralphniels on 08-06-15.
+ */
+public class BoardsTile extends Tile {
+    /**
+     * @param sprite The image which will be drawn whenever the draw method of the Tile is called.
+     */
+    public BoardsTile(Sprite sprite) {
+        super(sprite);
+    }
+}
